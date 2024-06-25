86d566c72398fb17cff1f70186ca1b1cfea19c9a
LesterTurra <lesterignacio.turra@alumnos.ulagos.cl>
2024-06-25 12:26:35 -0400

    Tarea7_Pregunta3.py

--- Tarea7_Pregunta3.py
+++ Tarea7_Pregunta3.py
@@ -1,19 +1,13 @@
 supermercado = {}
 
-print("Ingrese productos y sus cantidades (deje el nombre vacío para finalizar):")
-
-# Bucle para ingresar productos y cantidades hasta que se ingrese un nombre vacío
 while True:
-    nombre_producto = input("Nombre del producto: ")
-    if nombre_producto == "":
+    producto = input("Nombre del producto: ")
+    if producto == "":
         break
-    cantidad = int(input(f"Cantidad de {nombre_producto}: "))
-    supermercado[nombre_producto] = cantidad
+    cantidad = int(input("Ingrese la cantidad del producto : "))
+    supermercado[producto] = cantidad
 
-# Solicitar al usuario un valor X para multiplicar las cantidades
-x = int(input("Ingrese el valor por el cual multiplicar las cantidades: "))
+multiplicador = int(input("Ingrese el valor por el cual multiplicar las cantidades: "))
 
-# Mostrar los productos y sus cantidades multiplicadas por X
-print("Productos y cantidades multiplicadas:")
-for producto, cantidad in supermercado.items():
-    print(f"{producto}: {cantidad * x}")
+for x in supermercado:
+    print("producto: ", x ," cantidad: ", supermercado[x] * multiplicador)
