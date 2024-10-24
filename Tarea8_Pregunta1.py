28f33a293b23607d03e7ebf765a160d2045f76fe
LesterTurra <lesterignacio.turra@alumnos.ulagos.cl>
2024-10-24 00:30:04 -0300

    Delete Tarea8_Pregunta1.py

--- Tarea8_Pregunta1.py
+++
@@ -1,8 +0,0 @@
-def area_rectangulo(base, altura):
-    return base * altura
-
-base = 15
-altura= 10
-area = area_rectangulo(base, altura)
-
-print("El area del rectangulo con base", base ,"y altura", altura, "es", area )
